d47535ebb286a51d27529958720cfdc6dd07c4a5
mcather <14895427+MattCatz@users.noreply.github.com>
2019-02-18 16:53:34 -0600

    Testing subtraction

--- components/ALU/ALU_test.py
+++ components/ALU/ALU_test.py
@@ -4,6 +4,18 @@ from pymtl import *
 from pclib.test import run_test_vector_sim
 from ALU import ALU
 
+def alu_gen_random( o, f ):
+	test_vector_table=[('op1 op2 operation result*')]
+	last_result = 0x0
+	for i in xrange(40):
+		rand_1 = Bits(16, random.randint( -0x8000,0x7FFF ))
+		rand_2 = Bits(16, random.randint( -0x8000,0x7FFF ))
+		test_vector_table.append( [rand_1, rand_2, o, last_result] )
+		last_result = Bits( 16, f( rand_1, rand_2 ))
+	return test_vector_table
+
+	
+
 def test_add_fixed( dump_vcd ):
 	run_test_vector_sim( ALU( 16 ), [ 
 	('op1     op2 operation result*'),
@@ -16,16 +28,27 @@ def test_add_fixed( dump_vcd ):
 	] , dump_vcd)
 
 
-def test_add_random( dump_vcd ):
-	assert 0
+def test_add_random(dump_vcd):
+	f = lambda x,y: x+y
+	test_vector_table = alu_gen_random(0x1,f)
+	run_test_vector_sim( ALU(16), test_vector_table, dump_vcd )
 
 
 def test_sub_fixed( dump_vcd ):
-	assert 0
+	run_test_vector_sim( ALU( 16 ), [ 
+	('op1     op2 operation result*'),
+	[ 0x1,    0x1,      0x2,     0x0],
+	[ 0x3,    0x4,      0x2,     0x0],
+	[ 0xFF00, 0x00FF,   0x2,     0xFFFF],
+	[ 0x2,    0xFFFE,   0x2,     0xFE01],
+	[ 0x0,    0x0,      0x2,     0x4],
+	] , dump_vcd)
 
 
 def test_sub_random( dump_vcd ):
-	assert 0
+	f = lambda x,y: x-y
+	test_vector_table = alu_gen_random( 0x2, f )
+	run_test_vector_sim( ALU(16), test_vector_table, dump_vcd )
 
 
 def test_and_fixed( dump_vcd ):
