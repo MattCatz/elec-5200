2e0bb075891e0b93ee1f4abeb0d760f3ac536458
mcather <14895427+MattCatz@users.noreply.github.com>
2019-03-23 13:00:05 -0500

    Fixed mixup with register file storage

--- components/RegFile/RegFile_sv_test.py
+++ components/RegFile/RegFile_sv_test.py
@@ -8,3 +8,8 @@ from RegFile_test import (gen_data, gen_vector_table, static_test_vector)
 
 def test_rw( dump_vcd ):
 	run_test_vector_sim( RegFile_sv(dtype=16,nregs=16), static_test_vector, dump_vcd)
+
+def test_rw_sequential( dump_vcd ):
+	data = gen_data(16,8)
+	test_vector_table = gen_vector_table(data)
+	run_test_vector_sim( RegFile_sv(dtype=16,nregs=8), test_vector_table, dump_vcd )
--- components/RegFile/regfile.v
+++ components/RegFile/regfile.v
@@ -1,7 +1,7 @@
 module regfile
 #(parameter dtype = 16,
             nregs = 8,
-            addr_len = 3)
+            addr_len = $clog2(nregs))
 (input clock,
  input clk_en,
  input reset,
@@ -13,13 +13,11 @@ module regfile
  input [dtype-1:0] rZ);
 
 // Register file storage
-logic [nregs-1:0] registers [dtype-1:0];
-
-integer i;
+logic [dtype-1:0] registers [nregs-1:0];
 
 // Read and write from register file
 always_ff @(posedge clock)
-        if (clk_en) registers[rZ_address] <= rZ_address > 0 ? rZ : 0;
+        if (clk_en) registers[rZ_address] <= |rZ_address ? rZ : 0;
 
 assign rX = registers[rX_address];
 assign rY = registers[rY_address];
