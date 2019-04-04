4c07529e4c39057179cd7de6ac3081056708cced
mattcatz <14895427+MattCatz@users.noreply.github.com>
2019-04-04 15:10:06 -0500

    Fixed ALU tests

--- components/ALU/ALU_sv_test.py
+++ components/ALU/ALU_sv_test.py
@@ -4,123 +4,63 @@ import pytest
 from pymtl import *
 from pclib.test import run_test_vector_sim
 from ALU import alu as ALU_SV
-from ALU_test import alu_gen_random
 
-fixed_add_vector = [ 
-   ('op1     op2 operation result*'),
-   [ 0x1,    0x1,      0x1,     0x0],
-   [ 0x3,    0x4,      0x1,     0x2],
-   [ 0xFF00, 0x00FF,   0x1,     0x7],
-   [ 0x2,    0xFFFE,   0x1,     0xFFFF],
-   [ 0x2,    0xFFFF,   0x1,     0x0],
-   [ 0x0,    0x0,      0x1,     0x1],
-   ]
-
-fixed_sub_vector = [ 
-   ('op1     op2 operation result*'),
-   [ 0x1,    0x1,      0x2,     0x0],
-   [ 0x3,    0x4,      0x2,     0x0],
-   [ 0xFF00, 0x00FF,   0x2,     0xFFFF],
-   [ 0x2,    0xFFFE,   0x2,     0xFE01],
-   [ 0x0,    0x0,      0x2,     0x4],
-   ]
-
-fixed_and_vector = [ 
-   ('op1     op2 operation result*'),
-   [ 0x1,    0x1,      0x3,     0x0],
-   [ 0x3,    0x4,      0x3,     0x1],
-   [ 0xFF00, 0x00FF,   0x3,     0x0],
-   [ 0x2,    0xFFFE,   0x3,     0x0],
-   [ 0xA,    0xA,      0x3,     0x2],
-   [ 0x0,    0x0,      0x0,     0xA],
-   ]
-
-fixed_or_vector = [ 
-   ('op1     op2 operation result*'),
-   [ 0x1,    0x1,      0x4,     0x0],
-   [ 0x3,    0x4,      0x4,     0x1],
-   [ 0xFF00, 0x00FF,   0x4,     0x7],
-   [ 0x2,    0xFFFE,   0x4,     0xFFFF],
-   [ 0xA,    0x5,      0x4,     0xFFFE],
-   [ 0x0,    0x0,      0x0,     0xF],
-   ]
-
-fixed_LT_vector = [ 
-   ('op1     op2 operation result*'),
-   [ 0x1,    0x1,      0x5,     0x0],
-   [ 0x3,    0x4,      0x5,     0x0],
-   [ 0xFF00, 0x00FF,   0x5,     0x1],
-   [ 0x2,    0xFFFE,   0x5,     0x0],
-   [ 0xA,    0x5,      0x5,     0x1],
-   [ 0x0,    0x0,      0x0,     0x0],
-   ]
-
-fixed_ET_vector = [ 
-   ('op1     op2 operation result*'),
-   [ 0x1,    0x1,      0x6,     0x0],
-   [ 0x3,    0x4,      0x6,     0x1],
-   [ 0xFF00, 0x00FF,   0x6,     0x0],
-   [ 0x2,    0xFFFE,   0x6,     0x0],
-   [ 0xA,    0xA,      0x6,     0x0],
-   [ 0x0,    0x0,      0x0,     0x1],
-   ]
-
-random_add_vector = alu_gen_random(0x1, lambda x,y: x+y)
-random_sub_vector = alu_gen_random(0x2, lambda x,y: x-y)
-random_and_vector = alu_gen_random(0x3, lambda x,y: x&y)
-random_or_vector  = alu_gen_random(0x4, lambda x,y: x|y)
-random_LT_vector  = alu_gen_random(0x5, lambda x,y: x<y)
-random_ET_vector  = alu_gen_random(0x6, lambda x,y: x==y)
+from ALU_test import (fixed_add_vector, random_add_vector)
+from ALU_test import (fixed_sub_vector, random_sub_vector)
+from ALU_test import (fixed_and_vector, random_and_vector)
+from ALU_test import (fixed_or_vector, random_or_vector)
+from ALU_test import (fixed_LT_vector, random_LT_vector)
+from ALU_test import (fixed_ET_vector, random_ET_vector)
 
 def test_add_fixed( dump_vcd ):
-   run_test_vector_sim( ALU( 16 ), fixed_add_vector , dump_vcd)
+   run_test_vector_sim( ALU_SV( 16 ), fixed_add_vector , dump_vcd)
 
 
 def test_add_random(dump_vcd):
-   run_test_vector_sim( ALU(16), random_add_vector, dump_vcd )
+   run_test_vector_sim( ALU_SV(16), random_add_vector, dump_vcd )
 
 
 def test_sub_fixed( dump_vcd ):
-   run_test_vector_sim( ALU( 16 ), fixed_sub_vector , dump_vcd)
+   run_test_vector_sim( ALU_SV( 16 ), fixed_sub_vector , dump_vcd)
 
 
 def test_sub_random( dump_vcd ):
-   run_test_vector_sim( ALU(16), test_vector_table, dump_vcd )
+   run_test_vector_sim( ALU_SV(16), test_vector_table, dump_vcd )
 
 
 def test_and_fixed( dump_vcd ):
-   run_test_vector_sim( ALU( 16 ), fixed_and_vector , dump_vcd)
+   run_test_vector_sim( ALU_SV( 16 ), fixed_and_vector , dump_vcd)
 
 
 def test_and_random( dump_vcd ):
-   run_test_vector_sim( ALU(16), test_vector_table, dump_vcd )
+   run_test_vector_sim( ALU_SV(16), test_vector_table, dump_vcd )
 
 
 def test_or_fixed( dump_vcd ):
-   run_test_vector_sim( ALU( 16 ), fixed_or_vector , dump_vcd)
+   run_test_vector_sim( ALU_SV( 16 ), fixed_or_vector , dump_vcd)
 
 
 def test_or_random( dump_vcd ):
-   run_test_vector_sim( ALU(16), test_vector_table, dump_vcd )
+   run_test_vector_sim( ALU_SV(16), test_vector_table, dump_vcd )
 
 
 def test_less_than_fixed( dump_vcd ):
-   run_test_vector_sim( ALU( 16 ), fixed_LT_vector , dump_vcd)
+   run_test_vector_sim( ALU_SV( 16 ), fixed_LT_vector , dump_vcd)
 
 
 def test_less_than_random( dump_vcd ):
-   run_test_vector_sim( ALU(16), test_vector_table, dump_vcd )
+   run_test_vector_sim( ALU_SV(16), test_vector_table, dump_vcd )
 
 
 def test_equal_fixed( dump_vcd ):
-   run_test_vector_sim( ALU( 16 ), fixed_ET_vector , dump_vcd)
+   run_test_vector_sim( ALU_SV( 16 ), fixed_ET_vector , dump_vcd)
 
 
 def test_equal_random( dump_vcd ):
-   run_test_vector_sim( ALU(16), test_vector_table, dump_vcd )
+   run_test_vector_sim( ALU_SV(16), test_vector_table, dump_vcd )
 
 
 def test_invalid_operation( dump_vcd ):
-   run_test_vector_sim( ALU(16), test_vector_table, dump_vcd )
+   run_test_vector_sim( ALU_SV(16), test_vector_table, dump_vcd )
 
 
--- components/ALU/ALU_test.py
+++ components/ALU/ALU_test.py
@@ -8,133 +8,124 @@ from ALU import ALU
 
 def alu_gen_random( o, f ):
    test_vector_table=[('op1 op2 operation result*')]
-   last_result = 0x0
    for i in xrange(40):
       rand_1 = Bits(16, random.randint( -0x8000,0x7FFF ))
       rand_2 = Bits(16, random.randint( -0x8000,0x7FFF ))
-      test_vector_table.append( [rand_1, rand_2, o, last_result] )
-      last_result = Bits( 16, f( rand_1, rand_2 ))
+      result = Bits( 16, f( rand_1, rand_2 ))
+      test_vector_table.append( [rand_1, rand_2, o, result] )
+      
    return test_vector_table
 
-   
+
+fixed_add_vector = [ 
+   ('op1     op2 operation result*'),
+   [ 0x1,    0x1,      0x1,     0x2],
+   [ 0x3,    0x4,      0x1,     0x7],
+   [ 0xFF00, 0x00FF,   0x1,     0xFFFF],
+   [ 0x2,    0xFFFE,   0x1,     0x0],
+   [ 0x2,    0xFFFF,   0x1,     0x1]
+   ]
+
+fixed_sub_vector = [ 
+   ('op1     op2 operation result*'),
+   [ 0x1,    0x1,      0x2,     0x0],
+   [ 0x3,    0x4,      0x2,     0xFFFF],
+   [ 0xFF00, 0x00FF,   0x2,     0xFE01],
+   [ 0x2,    0xFFFE,   0x2,     0x4]
+   ]
+
+fixed_and_vector = [ 
+   ('op1     op2 operation result*'),
+   [ 0x1,    0x1,      0x3,     0x1],
+   [ 0x3,    0x4,      0x3,     0x0],
+   [ 0xFF00, 0x00FF,   0x3,     0x0],
+   [ 0x2,    0xFFFE,   0x3,     0x2],
+   [ 0xA,    0xA,      0x3,     0xA],
+   ]
+
+fixed_or_vector = [ 
+   ('op1     op2 operation result*'),
+   [ 0x1,    0x1,      0x4,     0x1],
+   [ 0x3,    0x4,      0x4,     0x7],
+   [ 0xFF00, 0x00FF,   0x4,     0xFFFF],
+   [ 0x2,    0xFFFE,   0x4,     0xFFFE],
+   [ 0xA,    0x5,      0x4,     0xF],
+   ]
+
+fixed_LT_vector = [ 
+   ('op1     op2 operation result*'),
+   [ 0x1,    0x1,      0x5,     0x0],
+   [ 0x3,    0x4,      0x5,     0x1],
+   [ 0xFF00, 0x00FF,   0x5,     0x0],
+   [ 0x2,    0xFFFE,   0x5,     0x1],
+   [ 0xA,    0x5,      0x5,     0x0],
+   ]
+
+fixed_ET_vector = [ 
+   ('op1     op2 operation result*'),
+   [ 0x1,    0x1,      0x6,     0x1],
+   [ 0x3,    0x4,      0x6,     0x0],
+   [ 0xFF00, 0x00FF,   0x6,     0x0],
+   [ 0x2,    0xFFFE,   0x6,     0x0],
+   [ 0xA,    0xA,      0x6,     0x1],
+   ]
+
+random_add_vector = alu_gen_random(0x1, lambda x,y: x+y)
+random_sub_vector = alu_gen_random(0x2, lambda x,y: x-y)
+random_and_vector = alu_gen_random(0x3, lambda x,y: x&y)
+random_or_vector  = alu_gen_random(0x4, lambda x,y: x|y)
+random_LT_vector  = alu_gen_random(0x5, lambda x,y: x<y)
+random_ET_vector  = alu_gen_random(0x6, lambda x,y: x==y)
+
 
 def test_add_fixed( dump_vcd ):
-   print "Testing Add with fixed imput"
-   run_test_vector_sim( ALU( 16 ), [ 
-   ('op1     op2 operation result*'),
-   [ 0x1,    0x1,      0x1,     0x0],
-   [ 0x3,    0x4,      0x1,     0x2],
-   [ 0xFF00, 0x00FF,   0x1,     0x7],
-   [ 0x2,    0xFFFE,   0x1,     0xFFFF],
-   [ 0x2,    0xFFFF,   0x1,     0x0],
-   [ 0x0,    0x0,      0x1,     0x1],
-   ] , dump_vcd)
+   run_test_vector_sim( ALU( 16 ), fixed_add_vector, dump_vcd)
 
 
 def test_add_random(dump_vcd):
-   print "Testing Add with random imput"
-   f = lambda x,y: x+y
-   test_vector_table = alu_gen_random(0x1,f)
-   run_test_vector_sim( ALU(16), test_vector_table, dump_vcd )
+   run_test_vector_sim( ALU(16), random_add_vector, dump_vcd )
 
 
 def test_sub_fixed( dump_vcd ):
-   print "Testing Subtract with fixed imput"
-   run_test_vector_sim( ALU( 16 ), [ 
-   ('op1     op2 operation result*'),
-   [ 0x1,    0x1,      0x2,     0x0],
-   [ 0x3,    0x4,      0x2,     0x0],
-   [ 0xFF00, 0x00FF,   0x2,     0xFFFF],
-   [ 0x2,    0xFFFE,   0x2,     0xFE01],
-   [ 0x0,    0x0,      0x2,     0x4],
-   ] , dump_vcd)
+   run_test_vector_sim( ALU( 16 ), fixed_sub_vector, dump_vcd)
 
 
 def test_sub_random( dump_vcd ):
-   print "Testing Subtract with random imput"
-   f = lambda x,y: x-y
-   test_vector_table = alu_gen_random( 0x2, f )
-   run_test_vector_sim( ALU(16), test_vector_table, dump_vcd )
+   run_test_vector_sim( ALU(16), random_sub_vector, dump_vcd )
 
 
 def test_and_fixed( dump_vcd ):
-   print "Testing And with fixed imput"
-   run_test_vector_sim( ALU( 16 ), [ 
-   ('op1     op2 operation result*'),
-   [ 0x1,    0x1,      0x3,     0x0],
-   [ 0x3,    0x4,      0x3,     0x1],
-   [ 0xFF00, 0x00FF,   0x3,     0x0],
-   [ 0x2,    0xFFFE,   0x3,     0x0],
-   [ 0xA,    0xA,      0x3,     0x2],
-   [ 0x0,    0x0,      0x0,     0xA],
-   ] , dump_vcd)
+   run_test_vector_sim( ALU( 16 ), fixed_and_vector, dump_vcd)
 
 
 def test_and_random( dump_vcd ):
-   print "Testing Logical And with random imput"
-   f = lambda x,y: x & y
-   test_vector_table = alu_gen_random( 0x3, f )
-   run_test_vector_sim( ALU(16), test_vector_table, dump_vcd )
+   run_test_vector_sim( ALU(16), random_and_vector, dump_vcd )
 
 
 def test_or_fixed( dump_vcd ):
-   print "Testing Or with fixed imput"
-   run_test_vector_sim( ALU( 16 ), [ 
-   ('op1     op2 operation result*'),
-   [ 0x1,    0x1,      0x4,     0x0],
-   [ 0x3,    0x4,      0x4,     0x1],
-   [ 0xFF00, 0x00FF,   0x4,     0x7],
-   [ 0x2,    0xFFFE,   0x4,     0xFFFF],
-   [ 0xA,    0x5,      0x4,     0xFFFE],
-   [ 0x0,    0x0,      0x0,     0xF],
-   ] , dump_vcd)
+   run_test_vector_sim( ALU( 16 ), fixed_or_vector, dump_vcd)
 
 
 def test_or_random( dump_vcd ):
-   print "Testing Logical Or with random imput"
-   f = lambda x,y: x | y
-   test_vector_table = alu_gen_random( 0x4, f )
-   run_test_vector_sim( ALU(16), test_vector_table, dump_vcd )
+   run_test_vector_sim( ALU(16), random_or_vector, dump_vcd )
 
 
 def test_less_than_fixed( dump_vcd ):
    print "Testing Less than with fixed imput"
-   run_test_vector_sim( ALU( 16 ), [ 
-   ('op1     op2 operation result*'),
-   [ 0x1,    0x1,      0x5,     0x0],
-   [ 0x3,    0x4,      0x5,     0x0],
-   [ 0xFF00, 0x00FF,   0x5,     0x1],
-   [ 0x2,    0xFFFE,   0x5,     0x0],
-   [ 0xA,    0x5,      0x5,     0x1],
-   [ 0x0,    0x0,      0x0,     0x0],
-   ] , dump_vcd)
+   run_test_vector_sim( ALU( 16 ), fixed_LT_vector, dump_vcd)
 
 
 def test_less_than_random( dump_vcd ):
-   print "Testing Less Than with random imput"
-   f = lambda x,y: x<y
-   test_vector_table = alu_gen_random( 0x5, f )
-   run_test_vector_sim( ALU(16), test_vector_table, dump_vcd )
+   run_test_vector_sim( ALU(16), random_LT_vector, dump_vcd )
 
 
 def test_equal_fixed( dump_vcd ):
    print "Testing Equals with fixed imput"
-   run_test_vector_sim( ALU( 16 ), [ 
-   ('op1     op2 operation result*'),
-   [ 0x1,    0x1,      0x6,     0x0],
-   [ 0x3,    0x4,      0x6,     0x1],
-   [ 0xFF00, 0x00FF,   0x6,     0x0],
-   [ 0x2,    0xFFFE,   0x6,     0x0],
-   [ 0xA,    0xA,      0x6,     0x0],
-   [ 0x0,    0x0,      0x0,     0x1],
-   ] , dump_vcd)
+   run_test_vector_sim( ALU( 16 ), fixed_ET_vector, dump_vcd)
 
 
 def test_equal_random( dump_vcd ):
-   print "Testing Equivalence with random imput"
-   f = lambda x,y: x == y
-   test_vector_table = alu_gen_random( 0x6, f )
-   run_test_vector_sim( ALU(16), test_vector_table, dump_vcd )
+   run_test_vector_sim( ALU(16), random_ET_vector, dump_vcd )
 
 
 def test_invalid_operation( dump_vcd ):
