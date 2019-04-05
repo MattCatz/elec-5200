efde39340e8c8f7e412b8a7d511fc455c9fc1f27
mattcatz <14895427+MattCatz@users.noreply.github.com>
2019-04-05 14:00:18 -0500

    Correcting tests

--- components/Datapath/Datapath_test.py
+++ components/Datapath/Datapath_test.py
@@ -1,5 +1,6 @@
 import random
 import pytest
+import copy
 
 from pymtl import *
 from pclib.test import run_test_vector_sim
@@ -22,6 +23,37 @@ class Datapath( VerilogModel ):
       s.word_a = OutPort(10)
 
 
+
+
+template_inputs = {
+   "reset": 0x0,
+   "regfile_w": 0x0,
+   "rX_address": 0x0,
+   "rY_address": 0x0,
+   "rZ_address": 0x0,
+   "alu_s": 0x0,
+   "data_s": 0x0,
+   "operand_s": 0x0,
+   "pc_s": 0x0,
+   "word_r": 0x0,
+   "immediate": 0x0,
+   "pc*": 0x0,
+   "word_w*": 0x0,
+   "word_a*": 0x0,
+}
+
+def gen_table_header( inputs ):
+   output = ""
+   for key, value in inputs:
+      output = output + key + ' '
+   return output
+
+def gen_table_row( inputs ):
+   output = []
+   for key, value in inputs:
+      output.append(value)
+   return output
+
 top_signals = " reset regfile_w "
 control_signals = " alu_s data_s operand_s pc_s "
 decoder_signals = " rX_address rY_address rZ_address immediate "
@@ -29,10 +61,13 @@ external_memory_signals = " word_r "
 
 signals_to_external_memory = " pc* word_w* word_a* "
 
-
 def test_simple( dump_vcd ):
    model = Datapath()
    model._auto_init()
+
+
+def test_gen_inputs():
+   print gen_table_header(template_inputs)
 
 
 def test_LW( dump_vcd ):
@@ -61,11 +96,23 @@ def test_RR_type( dump_vcd ):
    RR_memory  = [0x0]
    RR_outputs = [0x2, 0x0, 0x0]
 
+   temp = copy.deepcopy(template_inputs)
+
+   temp['regfile_w'] = 0x1
+   temp['alu_s'] = 0x1
+   temp['data_s'] = 0x1
+
+   temp['rX_address'] = 0x1
+   temp['rY_address'] = 0x2
+   temp['rZ_address'] = 0x3
+
+   temp['pc*'] = 0x2
+
    model = Datapath()
    model._auto_init()
 
-   test_vector = [(top_signals + control_signals + decoder_signals + external_memory_signals + signals_to_external_memory)]
-   test_vector.append(fixed_top_signals + RR_control + RR_decoder + RR_memory + RR_outputs)
+   test_vector = [gen_table_header(temp)]
+   test_vector.append(gen_table_row(temp))
 
    run_test_vector_sim(model, test_vector, dump_vcd)
 
