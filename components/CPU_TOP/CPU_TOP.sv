474ce98f5bfc58016b3804a23b28594b475a2811
mcather <14895427+MattCatz@users.noreply.github.com>
2019-04-05 22:43:21 -0500

    Bump

--- components/CPU_TOP/CPU_TOP.sv
+++ components/CPU_TOP/CPU_TOP.sv
@@ -1,32 +1,49 @@
-import constants::data_s_t;
-import constants::alu_func_t;
+`include "Constants.sv"
 
 module CPU_TOP(
 input clock,
-input reset
+input reset,
+input logic [2:0] inr,
+input debug,
+output logic [15:0] outvalue
 );
+
+import constants::data_s_t;
+import constants::alu_func_t;
+import constants::pc_s_t;
+import constants::operand_s_t;
 
 logic [1:0] operation;
 logic [2:0] funct;
-logic [2:0] pc_s;
-logic [2:0] operand_s;
+pc_s_t pc_s;
+operand_s_t operand_s;
+logic data_w;
 alu_func_t alu_s;
 data_s_t data_s;
 
 logic clk_en;
 logic [2:0] rX_address,rY_address,rZ_address;
 logic [2:0] alu_ctr;
-logic [15:0] word_r;
-logic [5:0] immediate;
+logic [7:0] immediate;
 logic [11:0] pc;
 
-logic write_A, write_B;
+logic write_B;
 logic [9:0] Address_A, Address_B;
 logic [15:0] Data_A, Data_B;
 logic [15:0] Out_A, Out_B;
 
+logic [2:0] debug_mux [1:0];
+
 assign Address_B = pc;
 assign write_B = 0;
+assign Data_B = 0;
+
+assign debug_mux[0] = rX_address;
+assign debug_mux[1] = inr;
+
+assign outvalue = Data_A;
+
+assign clk_en = 1'b1;
 
 Decoder decoder (
     .word(Out_B),
@@ -43,27 +60,30 @@ ControlUnit control_Unit (
     .pc_s(pc_s),
     .operand_s(operand_s),
     .alu_s(alu_s),
-    .data_s(data_s));
+    .data_s(data_s),
+    .data_w(data_w));
  
 Datapath data_Path (
-   .clock(clock),
+   .clk(clock),
    .reset(reset),
-   .clk_en(clk_en),
-   .rX_address(rX_address),
+   .regfile_w(clk_en),
+   .rX_address(debug_mux[debug]),
    .rY_address(rY_address),
    .rZ_address(rZ_address),
-   .alu_ctr(alu_s),
+   .alu_s(alu_s),
    .data_s(data_s),
    .operand_s(operand_s),
    .pc_s(pc_s),
-   .word_r(word_r),
+   .word_r(Out_A),
    .immediate(immediate),
-   .pc(pc));
+   .pc(pc),
+   .word_w(Data_A),
+   .word_a(Address_A));
    
 Memory external_mem (
     .clock(clock), 
     .clk_en(clk_en),
-    .write_A(write_A),
+    .write_A(data_w),
     .write_B(write_B),
     .Address_A(Address_A),
     .Address_B(Address_B),
@@ -73,4 +93,3 @@ Memory external_mem (
     .Out_B(Out_B));
 
 endmodule
-
