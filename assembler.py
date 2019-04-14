d692873b993b88fb8792bd135c1c204f9d6b0048
mcather <14895427+MattCatz@users.noreply.github.com>
2019-04-14 13:43:17 -0500

    Working on assembler

--- assembler.py
+++ assembler.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 
 import re
 import yaml
@@ -8,38 +8,30 @@ def get_isa(file_name):
 	ISA = None
 	with open(file_name, 'r') as stream:
 		try:
-			ISA = yaml.load(stream)
+			ISA = yaml.load(stream, Loader=yaml.FullLoader)
 		except tam.YAMLError as e:
 			print(e)
 	return ISA
 
 
-def pop_bits(value, split):
-	if value.bit_length() <= split:
-		return (value, 0)
-	mask = ((1 << split) - 1) << (value.bit_length() - split)
-	return ((value & mask) >> (value.bit_length() - split), value & ~mask)
+def pop_bits(value, splits):
+	for position, length in splits:
+		shift = value.bit_length() - length
+		next_yield = value
+		if shift >= 0:
+			mask = ((1 << length) - 1) << shift
+			next_yield = (value & mask) >> shift
+			value = value & ~mask
+		yield next_yield << (position - length + 1)
 
 
 def enumerate_syntax(instruction, ISA, symbol_table):
 	temp = copy.deepcopy(ISA['instructions'][instruction['memonic']])
 	temp['memonic'] = instruction['memonic']
-	temp['rx'] = ISA['registers'][instruction['rx']]
-	temp['rz'] = ISA['registers'][instruction['rz']]
-	if instruction['ry'] is not None and instruction['ry'] in ISA['registers']:
-		temp['ry'] = ISA['registers'][instruction['ry']]
-	if instruction['memonic'] == 'BEQ' and instruction['ry'] in symbol_table:
-		# This is a branch label
-		temp['kk'] = symbol_table[instruction['ry']]
-		temp['rx'] = temp['rz']
-		temp['ry'] = temp['rx']
-	if instruction['memonic'] == 'BEQ' and instruction['ry'] not in symbol_table:
-		# This is a branch address
-		temp['kk'] = instruction['ry']
-		temp['rx'] = temp['rz']
-		temp['ry'] = temp['rx']
-	if instruction['kk'] is not None:
-		temp['kk'] = int(instruction['kk'])
+	print(instruction)
+	print(type(instruction['kk']))
+	for key, value in instruction.items():
+		temp[key] = symbol_table[value]
 	return temp
 
 
@@ -48,11 +40,8 @@ def encode_instruction(tokens, ISA, symbol_table):
 	for fields in tokens['format']:
 		field_name = fields.pop(0)
 		value = tokens[field_name]
-		for start, length in fields:
-			section, value = pop_bits(value, length)
-			temp = temp + ((section << (start - length + 1)))
-		if value != 0:
-			raise ValueError('Value larger than field: {} {}'.format(value,length))
+		for split in pop_bits(value, fields):
+			temp += split
 	return format(temp, '016b')
 
 def main():
@@ -63,21 +52,19 @@ def main():
 	ry_or_kk_re = r'(?P<kk>\d+)?(?P<ry>\w+)?'
 	offset = r'?\[?(?P<offset>\d+)?\]?'
 	prog = re.compile(label_re + memonic_re + rz_re + rx_re + ry_or_kk_re)
-
 	pc = 0
-	symbol_table = {}
 	program = []
-	dissassembly = ""
-	hex_dump = ""
 
 	ISA = get_isa("isa.yaml")
+	symbol_table = ISA['registers']
 
 	# First Pass
 	with open("test.asm", 'r') as fp:
 		line = fp.readline()
 		while line:
 			match = prog.match(line).groupdict()
-			if match["label"]: symbol_table[match["label"]] = pc
+			if match["label"]: 
+				symbol_table[match["label"]] = pc
 			program.append(match)
 			pc = pc + 1
 			line = fp.readline()
